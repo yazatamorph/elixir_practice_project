f750391f45b1aeafb306e25465189b206ced4ee8
Zareen Price <yazatamorph@gmail.com>
2023-12-12 18:22:08 -0700

    Refactor module for reusability

--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -1,5 +1,6 @@
 defmodule DungeonCrawl.CLI.HeroChoice do
   alias Mix.Shell.IO, as: Shell
+  import DungeonCrawl.CLI.BaseCommands
 
   def start do
     Shell.cmd("clear")
@@ -18,29 +19,9 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     |> confirm_hero
   end
 
-  def display_options(options) do
-    options
-    |> Enum.with_index(1)
-    |> Enum.each(fn {option, index} ->
-      Shell.info("#{index} - #{option}")
-    end)
-    options
-  end
-
-  defp generate_question(options) do
-    options = Enum.join(1..Enum.count(options), ", ")
-    "Enter your selection: [#{options}]\n"
-  end
-
-  defp parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
-  end
-
   defp confirm_hero(choice) do
     Shell.cmd("clear")
     Shell.info(choice.description)
     if Shell.yes?("Is this correct?"), do: choice, else: start()
   end
-
 end
